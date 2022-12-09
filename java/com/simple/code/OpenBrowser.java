5df518d0c31d768018bb288b9a419bbf91c3df22
Shumson nahar <Shumson805@gmail.com>
2022-12-08 21:25:43 -0500

    Firsr commit

---
+++ java/com/simple/code/OpenBrowser.java
@@ -0,0 +1,14 @@
+package com.simple.code;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver = new ChromeDriver();
+		driver.get("https://google.com");
+	}
+
+}
